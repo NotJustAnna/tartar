fac9afe96cd55fb3b6ba1f5f25ee2f433ac80fea
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2020-07-17 23:30:01 -0300

    Let the error continue cascading instead of replacing with dumb EOF error

--- src/main/kotlin/net/notjustanna/tartar/extensions/ParserContext.kt
+++ src/main/kotlin/net/notjustanna/tartar/extensions/ParserContext.kt
@@ -7,11 +7,9 @@ import net.notjustanna.tartar.api.parser.SyntaxException
  * Ensures there's no character files after this block of code.
  */
 fun <R> ParserContext<*, *>.ensureEOF(block: () -> R): R {
-    try {
-        return block()
-    } finally {
-        if (!eof) throw SyntaxException("Should've reached end of content", eat().section)
-    }
+    val r = block()
+    if (!eof) throw SyntaxException("Should've reached end of content", eat().section)
+    return r
 }
 
 /**
